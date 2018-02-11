b13b35690ed87026115e33d8f957e7608fd73914
Weikang Bu <2288210b@student.gla.ac.uk>
2018-02-11 15:35:42 +0000

    Chapter 3

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context_dict = {'boldmessage': "Crunchy,creamy,cookie,candy,cupcake"}
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>")
+
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+                  url(r'^$', views.index, name='index'),
+                  url(r'rango/', include('rango.urls')),
+                  # above maps any URLs starting
+                  # with rango/ to be handled by
+                  # the rango application
+                  url(r'^admin/', admin.site.urls),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
