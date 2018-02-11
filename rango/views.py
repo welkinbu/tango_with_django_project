74850a6422615d2a5c1e79fb22cfe15437552e70
Weikang Bu <2288210b@student.gla.ac.uk>
2018-02-11 15:37:31 +0000

    Chapter 4

--- rango/views.py
+++ rango/views.py
@@ -8,5 +8,5 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>")
-
+    # return HttpResponse("Rango says here is the about page.")
+    return render(request, 'rango/about.html')
